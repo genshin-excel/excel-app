c7313064ab4ba79aefcaab2d28e2da0695fe78de
Pham-Xuan-Son <phamxuansons@gmail.com>
2023-03-17 20:54:17 +0700

    Change code with router

---
+++ src/teamPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TeamPage() {
+    return (
+        <div>
+            <h1>Team Page</h1>
+            <p>This is the team page.</p>
+        </div>
+    );
+}
+
+export default TeamPage;
