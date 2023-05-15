ab9f8146a61646ca2196d3d9a00713b971c91e33
Pham-Xuan-Son <phamxuansons@gmail.com>
2023-05-15 21:56:11 +0700

    fix render teampage

--- src/TestPage.tsx
+++ src/TestPage.tsx
@@ -1,78 +1,159 @@
-import React, { useState } from 'react';
-import { Box, FormControl, Select, MenuItem, Button, Grid, Container } from '@material-ui/core';
-import { Add } from '@material-ui/icons';
+// import React, { useState, useContext, useEffect, Suspense } from "react";
+import { Grid, Box, Container } from "@mui/material";
+// import { useParams, useNavigate } from "react-router-dom";
+// import { Team, emptyTeam } from "../../models/Team";
+// import { DBContext } from "../../database/Database";
+// import TeamDisplay from "../../components/TeamDisplay";
+// import { Add } from "@material-ui/icons";
+// import { FormControl, Select, MenuItem, Button } from "@material-ui/core";
 
-interface SelectedSkills {
-    [key: string]: string;
-}
+// interface SelectedSkills {
+//   [key: string]: string;
+// }
 
-interface LineState {
-    selectedSkills: SelectedSkills;
-}
+// interface LineState {
+//   selectedSkills: SelectedSkills;
+// }
+// interface TeamDetailsProps {
+//   team: Team;
+//   onDelete: () => void;
+//   onTeamChange: (newTeam: Team) => void;
+// }
 
-const DropdownsLine = () => {
-    const [lines, setLines] = useState<LineState[]>([{ selectedSkills: {} }]);
+// function TeamDetail({ team, onDelete, onTeamChange }: TeamDetailsProps) {
+//   const database = useContext(DBContext);
+//   const { teamIndex } = useParams();
+//   const navigate = useNavigate();
+// //   const [team, setTeam] = useState(emptyTeam);
+// //   const [lines, setLines] = useState<LineState[]>([{ selectedSkills: {} }]);
 
-    const skills = [
-        { id: 'skill1', options: ['None', 'Option 1', 'Option 2', 'Option 3'] },
-        { id: 'skill2', options: ['Option 4', 'Option 5', 'Option 6'] },
-        { id: 'skill3', options: ['Option 7', 'Option 8', 'Option 9'] },
-        { id: 'skill4', options: ['Option 10', 'Option 11', 'Option 12'] }
-    ];
+//   useEffect(() => {
+//     if (!teamIndex) {
+//       navigate("/");
+//       return;
+//     }
 
-    const handleSkillChange = (
-        event: React.ChangeEvent<{ value: unknown }>,
-        skillId: string,
-        lineIndex: number
-    ) => {
-        setLines((prevLines) => {
-            const updatedLines = [...prevLines];
-            updatedLines[lineIndex].selectedSkills = {
-                ...updatedLines[lineIndex].selectedSkills,
-                [skillId]: event.target.value as string
-            };
-            return updatedLines;
-        });
-    };
+//     // const fetchedTeam = database.getTeamDAO().getTeamByName(teamIndex) as Team;
+//     if (!fetchedTeam) {
+//       navigate("/");
+//       return;
+//     }
 
-    const handleNewLine = () => {
-        setLines((prevLines) => [...prevLines, { selectedSkills: {} }]);
-    };
+//     setTeam(fetchedTeam);
+//     console.log(fetchedTeam);
+//   }, [database, navigate, teamIndex]);
 
-    return (
-        <>
-            <Container style={{ padding: 0 }}>
-                {lines.map((line, lineIndex) => (
-                    <Box mb={2} key={lineIndex}>
-                        <Grid container spacing={2}>
-                            {skills.map(({ id, options }) => (
-                                <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
-                                    <FormControl fullWidth variant="outlined" style={{ margin: '10px 0' }}>
-                                        <Select
-                                            id={id}
-                                            value={line.selectedSkills[id] || ''}
-                                            onChange={(event) => handleSkillChange(event, id, lineIndex)}
-                                        >
-                                            {options.map((option, optionIndex) => (
-                                                <MenuItem key={optionIndex} value={option}>
-                                                    {option}
-                                                </MenuItem>
-                                            ))}
-                                        </Select>
-                                    </FormControl>
-                                </Grid>
-                            ))}
+//   const handleTeamChange = (newTeam: Team) => {
+//     navigate(`/TeamPage/${newTeam.name}`);
+//   };
+
+//   if (!teamIndex) {
+//     return null;
+//   }
+//   return (
+//     <Suspense fallback={null}>
+//       <TeamDisplay
+//         team={team}
+//         onDelete={onDelete}
+//         onTeamChange={onTeamChange}
+//       />
+//     </Suspense>
+//   );
+// }
+
+function TeamPage() {
+  
+//   console.log("Final: " + team.name);
+  // ===============================================
+  // const skills = [
+  //   { id: "skill1", options: ["None", "Option 1", "Option 2", "Option 3"] },
+  //   { id: "skill2", options: ["Option 4", "Option 5", "Option 6"] },
+  //   { id: "skill3", options: ["Option 7", "Option 8", "Option 9"] },
+  //   { id: "skill4", options: ["Option 10", "Option 11", "Option 12"] },
+  // ];
+
+  // const handleSkillChange = (
+  //   event: any,
+  //   skillId: string,
+  //   lineIndex: number
+  // ) => {
+  //   setLines((prevLines) => {
+  //     const updatedLines = [...prevLines];
+  //     const selectedSkills = { ...updatedLines[lineIndex].selectedSkills };
+  //     selectedSkills[skillId] = event.target.value as string;
+  //     updatedLines[lineIndex].selectedSkills = selectedSkills;
+  //     return updatedLines;
+  //   });
+  // };
+
+  // const handleNewLine = () => {
+  //   setLines((prevLines) => [...prevLines, { selectedSkills: {} }]);
+  // };
+
+  return (
+    <Container maxWidth="xl" sx={{ padding: 0 }}>
+      <Container maxWidth="xl">
+        <Box
+          sx={{ display: "flex", justifyContent: "center", margin: "26px 0" }}
+        >
+          <Grid container>
+            <Container maxWidth="xl">
+              <Grid container item spacing={2} xs={6}>
+                {/* {team && (
+                  <Suspense fallback={null}>
+                    <TeamDetail
+                      team={team}
+                      onDelete={() => navigate("/")}
+                      onTeamChange={handleTeamChange}
+                    />
+                  </Suspense>
+                )} */}
+                {/* <Grid container sx={{ marginTop: 1 }}>
+                  {lines.map((line, lineIndex) => (
+                    <Grid item container xs={12} spacing={2} key={lineIndex} sx={{ marginTop: 1 }}>
+                      {skills.map(({ id, options }) => (
+                        <Grid item xs={6} sm={3} key={id}>
+                          <FormControl fullWidth variant="outlined">
+                            <Select
+                              id={id}
+                              value={line.selectedSkills[id] || ""}
+                              onChange={(event) =>
+                                handleSkillChange(event, id, lineIndex)
+                              }
+                            >
+                              {options.map((option, optionIndex) => (
+                                <MenuItem key={optionIndex} value={option}>
+                                  {option}
+                                </MenuItem>
+                              ))}
+                            </Select>
+                          </FormControl>
                         </Grid>
-                    </Box>
-                ))}
-                <Grid container justifyContent="center" style={{ marginTop: '1rem' }}>
-                    <Button onClick={handleNewLine} startIcon={<Add />} variant="outlined">
-                        New Line
+                      ))}
+                    </Grid>
+                  ))}
+                  <Grid
+                    container
+                    justifyContent="center"
+                    style={{ marginTop: "1rem" }}
+                  >
+                    <Button
+                      onClick={handleNewLine}
+                      startIcon={<Add />}
+                      variant="outlined"
+                    >
+                      New Line
                     </Button>
-                </Grid>
+                  </Grid>
+                </Grid> */}
+              </Grid>
+              <Grid container item spacing={2} xs={6}></Grid>
             </Container>
-        </>
-    );
-};
+          </Grid>
+        </Box>
+      </Container>
+    </Container>
+  );
+}
 
-export default DropdownsLine;
+export default TeamPage;
